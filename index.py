d047a916cf783f5945bb2f7aae8ff96d616b963d
Dilshod Abdulloev <Dilshodabdullayev2003@gmail.com>
2022-09-08 20:28:43 +0500

    Add theory about ssh keys

---
+++ index.py
@@ -0,0 +1,12 @@
+# ssh-keygen -t rsa -b 4096 -C "Dilshodabdullayev2003@gmail.com"
+
+
+# ssh key generator
+# -t = type
+# -b = bits
+
+# ssh -T git@github.com
+# -T = tunnel connection to git@github.com
+
+# ! Public key = замочная скважина
+# ? Private key = ключ к замку
